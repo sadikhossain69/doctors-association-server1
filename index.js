c313f516beba09afd8889df8903312a7d58c2193
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-17 20:01:31 +0600

    verify admin setup

--- index.js
+++ index.js
@@ -106,21 +106,14 @@ async function run() {
             res.send({admin: isAdmin})
         })
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded?.email
-            const requsterAccount = await userCollection.findOne({ email: requester })
-            if (requsterAccount.role === 'admin') {
                 const filter = { email: email };
                 const updateDoc = {
                     $set: { role: 'admin' },
                 };
                 const result = await userCollection.updateOne(filter, updateDoc);
                 res.send(result);
-            }
-            else {
-                res.status(403).send({ message: 'forbidder' })
-            }
         })
 
         app.put('/user/:email', async (req, res) => {
